7004bc37fed9aa124de65740170b7d27871a4620
xiaopengtang <630787976@qq.com>
2018-01-23 00:24:50 +0800

    调试

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,14 @@
 export const message = {
   'host': '//1989591.51vip.biz:17015/http-bind/'
 }
+
+
+export const amap = {
+	'key': '8236443209b67c2e2c0ecdeb7ef315c0',
+	// "plugin": [], // 所需插件
+	"container": 'amap', //所在el的id
+	"option": {
+		'resizeEnable': true
+	},
+	// "Geolocation": {} // Geolocation配置
+}
