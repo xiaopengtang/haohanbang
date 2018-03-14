90f424e03d9d2f8f8abaccdcc82d47ec796bbbe4
853232271@qq.com <853232271@qq.com>
2018-03-14 23:01:31 +0800

    claen up

--- src/pages/addressControl/index.ts
+++ src/pages/addressControl/index.ts
@@ -13,22 +13,50 @@ export class AddressControlPage {
 
   private $http;
   public addressList: Array<object>;
+  public conuntys;
+  public provinces;
+  public citys;
+
+  public conunty;
+  public province;
+  public city;
 
   constructor(public navCtrl: NavController) {
     this.$http = http();
-    console.log(user);
-    // this.getAddressList();
+    // console.log(user);
+
+    this.conuntys = [
+      {
+        id:40002,
+        name: "中国"
+      }
+    ];
+    this.provinces = [];
+    this.citys = [];
+
+    this.conunty = this.conuntys[0];
+    this.provinceList();
   }
 
   // 获取常用地址列表
-  async getAddressList() {
+  async provinceList() {
     // console.log(1111);
-    // let list = await this.$http.curl('ADDRESS:LIST', {
-    //   "userId": user.state.id
-    // });
-    // if( 1 == list.code ){
-    //   this.addressList = list.data;
-    // }
-    // console.log(list);
+    // 省份列表
+    let province_rsp = await this.$http.curl('SUPPORT:queryByParentCode', {
+      "poiCode": 40002
+    });
+    this.provinces = province_rsp.data;
+    console.log(province_rsp);
+  }
+
+  async cityList() {
+    // console.log(1111);
+    console.log(this.province);
+    // 省份列表
+    let citys_rsp = await this.$http.curl('SUPPORT:queryByParentCode', {
+      "poiCode": this.province
+    });
+    this.citys = citys_rsp.data;
+
   }
 }
--- src/pages/serviceDetail/index.ts
+++ src/pages/serviceDetail/index.ts
@@ -11,73 +11,73 @@ import * as user from 'hhb-userauth'
 })
 
 export class SerivceDetailPage {
-	private orderId: string;
-	info: any = {};
-  private $http;
-	public $server: Service;
-	constructor(public alertCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams ,public modalCtrl: ModalController ,public $server: Service, private toastCtrl: ToastController) {
-        this.orderId = navParams.get('orderId')
-        this.$http = $http()
-    }
-
-    async ngAfterViewInit(){
-    	const res = await this.$server.queryDetail(this.orderId)
-    	console.log(res)
-    	return this.info = res.data || {}
-    }
-    reply(){
-    	let prompt = this.alertCtrl.create({
-	        title: '评论',
-	        // message: "Enter a name for this new album you're so keen on adding",
-	        inputs: [
-		        {
-		            name: 'message',
-		            placeholder: '请输入评论信息'
-		        },
-	        ],
-	        buttons: [
-	        {
-	            text: '取消',
-	            handler: (data) => {
-	                console.log('Cancel clicked');
-	            }
-	        },
-	        {
-	            text: '评论',
-	            handler: async(data) => {
-                const res = await this.$http.curl('SAVE:ORDER:APPLY_MESSAGE', {
-                  // "ip": "string",
-                  "message": data.message,
-                  "orderId": this.orderId,
-                  "userId": user.id
-                  })
-                // console.log(res)
-                let toast
-                if(res && res.code){
-                  toast = this.toastCtrl.create({
-                    message: '评论成功',
-                    duration: 2000,
-                    position: 'top'
-                  });
-	                // console.log('Saved clicked', data);
-                }else{
-                  toast = this.toastCtrl.create({
-                    message: '评论失败',
-                    duration: 2000,
-                    position: 'top'
-                  });
-                }
-                toast.present();
-	            }
-	        }]
-	    });
-	    prompt.present();
-    }
-
-	applyOrder(){
-		let orderId = this.orderId
-		let modal = this.modalCtrl.create(ModalApply, {orderId});
-        modal.present();
-
-	}
+	// private orderId: string;
+	// info: any = {};
+  // private $http;
+	// // public $server: Service;
+	// constructor(public alertCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams ,public modalCtrl: ModalController ,public $server: Service, private toastCtrl: ToastController) {
+  //       this.orderId = navParams.get('orderId')
+  //       this.$http = $http()
+  //   }
+  //
+  //   async ngAfterViewInit(){
+  //   	const res = await this.$server.queryDetail(this.orderId)
+  //   	console.log(res)
+  //   	return this.info = res.data || {}
+  //   }
+  //   reply(){
+  //   	let prompt = this.alertCtrl.create({
+	//         title: '评论',
+	//         // message: "Enter a name for this new album you're so keen on adding",
+	//         inputs: [
+	// 	        {
+	// 	            name: 'message',
+	// 	            placeholder: '请输入评论信息'
+	// 	        },
+	//         ],
+	//         buttons: [
+	//         {
+	//             text: '取消',
+	//             handler: (data) => {
+	//                 console.log('Cancel clicked');
+	//             }
+	//         },
+	//         {
+	//             text: '评论',
+	//             handler: async(data) => {
+  //               const res = await this.$http.curl('SAVE:ORDER:APPLY_MESSAGE', {
+  //                 // "ip": "string",
+  //                 "message": data.message,
+  //                 "orderId": this.orderId,
+  //                 "userId": user.id
+  //                 })
+  //               // console.log(res)
+  //               let toast
+  //               if(res && res.code){
+  //                 toast = this.toastCtrl.create({
+  //                   message: '评论成功',
+  //                   duration: 2000,
+  //                   position: 'top'
+  //                 });
+	//                 // console.log('Saved clicked', data);
+  //               }else{
+  //                 toast = this.toastCtrl.create({
+  //                   message: '评论失败',
+  //                   duration: 2000,
+  //                   position: 'top'
+  //                 });
+  //               }
+  //               toast.present();
+	//             }
+	//         }]
+	//     });
+	//     prompt.present();
+  //   }
+  //
+	// applyOrder(){
+	// 	let orderId = this.orderId
+	// 	let modal = this.modalCtrl.create(ModalApply, {orderId});
+  //       modal.present();
+  //
+	// }
 }
