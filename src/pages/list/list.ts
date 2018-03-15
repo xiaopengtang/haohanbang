25916bc1a9c48fa3f5ec2740112cff80c706d225
Unknown <630787976@qq.com>
2018-03-16 00:26:59 +0800

    03-15 03

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -6,32 +6,34 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
+  constructor(navCtrl: NavController, navparams: NavParams) {
+    for (var i = 0; i < 30; i++) {
+      this.items.push( this.items.length );
     }
   }
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
+
+  doInfinite(): Promise<any> {
+    console.log('Begin async operation');
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        for (var i = 0; i < 30; i++) {
+          this.items.push( this.items.length );
+        }
+
+        console.log('Async operation has ended');
+        resolve();
+      }, 500);
+    })
   }
 }
