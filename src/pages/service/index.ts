a0d7d6f1aac8c04401f92c9e9a0d73ec94ad5f11
tangxiaopeng <630787976@qq.com>
2018-03-15 16:55:48 +0800

    03-15

--- src/pages/service/index.ts
+++ src/pages/service/index.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as user from 'hhb-userauth'
 
 @Component({
   selector: 'page-service',
@@ -14,11 +15,34 @@ export class ServicePage {
 	    // "orderByField": null,
 	    // "orderId": null,
 	    "page": 1,
-	    "queryUserId": 1,
+	    "queryUserId": user.id,
 	    "size": 10,
 	    // "status": 0,
 	    // "title": null,
 	    // "userId": '0000000002'
+	}
+	doRefresh(refresher) {
+		// console.log('Begin async operation', refresher);
+	
+		setTimeout(() => {
+		//   console.log('Async operation has ended');
+		  this.map = {
+			"asc": true,
+			"page": 1,
+			"queryUserId": user.id,
+			"size": 10
+		  }
+		  refresher.complete();
+		}, 1000);
+	}
+	doInfinite(scroll){
+		// console.log('Begin async operation');
+		scroll.enable(false)
+		return setTimeout(() => {
+			// console.log('Async operation has ended');
+			this.map.page++
+			scroll.complete()
+		  }, 500);
 	}
 	constructor(public navCtrl: NavController) {}
 }
