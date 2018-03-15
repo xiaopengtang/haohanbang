7faab8855fef303a75e1c1c1f66dbd94f71bdba6
853232271@qq.com <853232271@qq.com>
2018-03-16 00:02:06 +0800

    clane up

--- src/pages/addressControl/index.ts
+++ src/pages/addressControl/index.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, Platform} from 'ionic-angular';
 
 import * as http from 'hhb-http'
 import * as user from 'hhb-userauth'
@@ -16,10 +16,12 @@ export class AddressControlPage {
   public conuntys;
   public provinces;
   public citys;
+  public areas;
 
   public conunty;
   public province;
   public city;
+  public area;
 
   constructor(public navCtrl: NavController) {
     this.$http = http();
@@ -27,12 +29,13 @@ export class AddressControlPage {
 
     this.conuntys = [
       {
-        id:40002,
+        id: 40002,
         name: "中国"
       }
     ];
     this.provinces = [];
     this.citys = [];
+    this.areas = [];
 
     this.conunty = this.conuntys[0];
     this.provinceList();
@@ -49,14 +52,48 @@ export class AddressControlPage {
     console.log(province_rsp);
   }
 
-  async cityList() {
-    // console.log(1111);
-    console.log(this.province);
-    // 省份列表
-    let citys_rsp = await this.$http.curl('SUPPORT:queryByParentCode', {
-      "poiCode": this.province
-    });
-    this.citys = citys_rsp.data;
+  async ChangeCity() {
 
+    if (this.citys.length == 0) {
+      // 省份列表
+      let citys_rsp = await this.$http.curl('SUPPORT:queryByParentCode', {
+        "poiCode": this.province
+      });
+      this.citys = citys_rsp.data;
+    }
+  }
+
+  async ChangeArea() {
+    if (this.areas.length == 0) {
+      // 省份列表
+      let areas_rsp = await this.$http.curl('SUPPORT:queryByParentCode', {
+        "poiCode": this.city
+      });
+      this.areas = areas_rsp.data;
+    }
+  }
+
+  async CheckNewItem(key, item) {
+    switch (key) {
+      case "province":
+        if( item.poiCode != this.province){
+          console.log(item);
+          let rsp = await this.$http.curl('SUPPORT:queryByParentCode', {
+            "poiCode": item.poiCode
+          });
+          this.citys = rsp.data;
+        }
+        break;
+      case "city":
+        if( item.poiCode != this.city){
+          let rsp = await this.$http.curl('SUPPORT:queryByParentCode', {
+            "poiCode": item.poiCode
+          });
+          this.areas = rsp.data;
+        }
+        break;
+      default:
+        break;
+    }
   }
 }
